9079cef695134c3f4c669a3cc617fa91106e5fe9
Ghali Dhobb <ghalidhobb@gmail.com>
2018-04-25 12:07:20 +0100

    Order controller Product shows

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,37 +5,50 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new(product: @product)
-    @owner = @order.product.user
-    authorize @order
   end
 
   def create
     @order = Order.new(order_params)
     @order.user = current_user
+    @number_of_days = Date.parse("#{@order.end_date}") - Date.parse("#{@order.start_date}")
+    @order.amount_cents = @product.price_cents * @number_of_days
     @order.save
-    redirect_to new_product_order_payment_path(@order)
+    redirect_to order_path(@order)
+    authorize @order
   end
 
   def edit
   end
 
   def show
-    order = Order.new(product: @product)
+    @order = Order.find(params[:id])
     @owner = @order.product.user
     authorize @order
   end
 
   def update
+
+    # charge = Stripe::Charge.create(
+  #   customer:     customer.id,   # You should store this customer id and re-use it.
+  #   amount:       @order.amount_cents,
+  #   description:  "Payment for product #{@order.product_id} for order #{@order.id}",
+  #   currency:     @order.amount.currency
+  # )
+  # @order.update(payment: charge.to_json, state: 'paid')
+  # redirect_to order_path(@order)
+
+  # rescue Stripe::CardError => e
+  # flash[:alert] = e.message
+  # redirect_to new_order_payment_path(@order)
   end
 
   private
 
   def order_params
-    params.require(:order).permit(:start_date, :end_date, :amount_cents, :product_id, :user_id)
+    params.require(:order).permit(:start_date, :end_date, :product_id, :user)
   end
 
   def set_product
-    @product = Product.find(params[:product_id])
+    @product = Product.find(order_params[:product_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   resources :products do
-    resources :orders, only: [:new, :create] do
-      resources :payments, only: [:new,:create]
-    end
     resources :reviews, only: [:index, :new, :create]
   end
-  resources :orders, only: [:index, :show, :edit, :update]
+  resources :orders, only: [:index, :show, :edit, :update, :create]
   resources :reviews, only: [:edit, :update, :destroy]
   devise_for :users
   root to: 'pages#home'
