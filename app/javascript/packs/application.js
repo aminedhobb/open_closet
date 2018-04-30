970b1f69dcbb9ec43335bde1c8bcadebc34700c3
Reda HALIB <redahalib@me.com>
2018-04-30 10:46:05 +0100

    js keyword rotating

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,13 @@
 import "bootstrap";
+
+var keywords = ["wedding", "party", "weekend escape", "festival"];
+var count = 1;
+setInterval(function(){
+    $("span.keyword").fadeOut(400, function(){
+        $(this).html(keywords[count]);
+        count++;
+        if(count == keywords.length)
+            count = 0;
+        $(this).fadeIn(400);
+    });
+}, 4000);
