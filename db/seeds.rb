5774b62455a6a5ad853fab1fd277bb54423d9d7f
Ghali Dhobb <ghalidhobb@gmail.com>
2018-05-03 11:34:33 +0100

    Seeding real products

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,78 +34,158 @@ puts "User created!"
 puts "Creating products.."
 products = [
   {
-    title: 'Pink Sweater',
-    category: 'Sweater',
-    description: 'All goys will love your amazing pink sweater',
+    title: 'Blazer Richard James',
+    category: 'Costume',
+    description: 'Sharp blazer, perfect for any occasion',
     address: '33 rue de lourmel Paris',
-    price_cents: '2000',
-    gender: 'Men',
-    user_id: User.first.id
-  },
-  {
-    title: 'Green Shirt',
-    category: 'Shirt',
-    description: 'You will seem very trendy with this green shirt',
-    address: 'rue imilchil Casablanca',
-    price_cents: '200',
-    gender: 'Unisex',
-    user_id: User.first.id
-  },
-  {
-    title: 'Louis Vuitton Bag',
-    category: 'Bag',
-    description: 'With this classic Louis Vuitton bag, you will get a lot of reviews from all jealous girls',
-    address: 'rue de restinga Casablanca',
     price_cents: '4000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Blue Boglioni blazer',
+    category: 'Costume',
+    description: 'Clean blue blazer, a classic.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Max Mara Blue Jacket',
+    category: 'Costume',
+    description: 'Suited for an interview, a working day, or a formal event.',
+    address: 'Rue de Temple, Paris',
+    price_cents: '41000',
     gender: 'Women',
     user_id: User.first.id
   },
   {
-    title: 'DC Shoes',
-    category: 'Shoes',
-    description: 'You will like this nice DC shoes',
-    address: 'rue curie Casablanca',
-    price_cents: '400',
+    title: 'Grey Blazer Isabel Marant',
+    category: 'Costume',
+    description: 'Modern Blazer',
+    address: '6 Avenue Marceau, Paris',
+    price_cents: '35000',
+    gender: 'Women',
+    user_id: User.first.id
+  },
+  {
+    title: 'Grey Suit Paul Smith',
+    category: 'Costume',
+    description: 'Costume perfectly suited for an interview if you want to look sharp.',
+    address: 'Rue Curie, Casablanca',
+    price_cents: '50000',
     gender: 'Men',
     user_id: User.first.id
   },
   {
-    title: 'Classic Suit',
-    category: 'Suit',
-    description: 'You will shine with this classic suit',
-    address: 'boulevard zerktouni Casablanca',
-    price_cents: '5000',
+    title: 'Green Boglioni Jacket',
+    category: 'Costume',
+    description: 'Original green blazer, great seasonnal look.',
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '40000',
     gender: 'Men',
     user_id: User.first.id
   },
   {
-    title: 'Nice Pant',
-    category: 'Pant',
-    description: 'You are the king of the night with this amazing pant',
-    address: 'rue deparcieux Paris',
-    price_cents: '400',
-    gender: 'Men',
+    title: 'Black Stella McCartney Coat',
+    category: 'Costume',
+    description: "Beautiful Coat, original and trendy, you won't go unnoticed.",
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '45000',
+    gender: 'Women',
     user_id: User.first.id
   },
   {
-    title: 'Crazy shirt',
-    category: 'Shirt',
-    description: 'You will never regret this crazy shirt',
-    address: 'rue de rennes Paris',
-    price_cents: '1000',
-    gender: 'Men',
+    title: 'Gucci Evening Dress',
+    category: 'Costume',
+    description: 'Magic Dress, you will shine and be the star of the evening.',
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '85000',
+    gender: 'Women',
     user_id: User.first.id
   },
   {
-    title: 'Prada bag',
+    title: 'Marchesa Evening Dress',
+    category: 'Costume',
+    description: 'The perfect dress to be remembered by everyone at the event.',
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '90000',
+    gender: 'Women',
+    user_id: User.first.id
+  },
+  {
+    title: 'Satin Dress Danielle Frankel',
+    category: 'Costume',
+    description: 'A really classy dress for special occasions.',
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '75000',
+    gender: 'Women',
+    user_id: User.first.id
+  },
+  {
+    title: 'Balenciaga Black Bag',
     category: 'Bag',
-    description: 'With this nice Prada bag, you will feel the rythm of the fashion week ;)',
-    address: 'New York',
-    price_cents: '2000',
-    gender: 'Women',
+    description: 'A true Classic.',
+    address: 'Rue Imilchil, Casablanca',
+    price_cents: '50000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Dolge & Gabbana Bag',
+    category: 'Bag',
+    description: 'Modern Bag.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '43000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Givenchy Black Bag',
+    category: 'Bag',
+    description: 'Beautiful Givenchy bag.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Prada Bag',
+    category: 'Bag',
+    description: 'Classic Prada bag.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Red Saint-Laurent Bag',
+    category: 'Bag',
+    description: 'Great practical bag.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Fendi Bag',
+    category: 'Bag',
+    description: 'Original Fendi bag',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
+    user_id: User.first.id
+  },
+  {
+    title: 'Linen Theory Jacket',
+    category: 'Costume',
+    description: 'Clean blazer, suited for any occasion.',
+    address: 'Rue Imilchil Casablanca',
+    price_cents: '45000',
+    gender: 'Men',
     user_id: User.first.id
   }
-
 ]
 
 Product.create!(products)
@@ -135,4 +215,16 @@ orders = [
 Order.create!(orders)
 
 puts "orders created !"
+
+puts "Seeding Products Images"
+
+blazer_beige_richard_james = Dir.entries("app/assets/images/blazer_beige_richard_james").select do |entry|
+  File.file?("app/assets/images/blazer_beige_richard_james/#{entry}")
+end.map  do |image_name|
+  Cloudinary::Uploader.upload("app/assets/images/blazer_beige_richard_james/#{image_name}")
+  end.map do |element|
+  "#{element["resource_type"]}/#{element["type"]}/v#{element["version"]}/#{element['public_id']}.#{element['format']}"
+  end
+
+
 
