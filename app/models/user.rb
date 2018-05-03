f4627cce5536f9eb6d1800e1bfe432706e745398
Ghali Dhobb <ghalidhobb@gmail.com>
2018-05-03 17:01:18 +0100

    Revert " seed & avatar"
    This reverts commit 8fc8081707eacabe5d7fdfdddbaefefd7a48481f.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true
+  mount_uploader :avatar, AvatarUploader
 
   private
 
